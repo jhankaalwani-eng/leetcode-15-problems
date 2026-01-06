61393cdf1a71929a6cc849193fb91af7e4a6027a
Janka <jhankaalwani@gmail.com>
2026-01-06 17:13:01 +0500

    Create ImplementStackUsingQueue.java

---
+++ ImplementStackUsingQueue.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    Queue<Integer> q = new LinkedList<>();
+
+    public void push(int x) {
+        q.add(x);
+        int size = q.size();
+        for (int i = 1; i < size; i++) {
+            q.add(q.remove());
+        }
+    }
+
+    public int pop() {
+        return q.remove();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
